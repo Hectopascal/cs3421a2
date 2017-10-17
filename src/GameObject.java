9650585f02e48ba8c5f2e1d011f6a38b31c3344e
Gary Chen <cpldhycxc@gmail.com>
2017-10-17 11:35:04 +1100

    game object?

--- src/GameObject.java
+++ src/GameObject.java
@@ -3,7 +3,19 @@ import java.util.ArrayList;
 public class GameObject {
     public  ArrayList<GameObject> all_object = new ArrayList<GameObject>();
     public static final GameObject ROOT = new GameObject(null);
+    private GameObject parent;
+    private ArrayList<GameObject> children = new ArrayList<GameObject>();
+    // constructor
+    public GameObject (GameObject parent){
+        this.parent = parent;
+        if (parent != null){
+            this.parent.children.add(this);
+        }
+        this.children = new ArrayList<GameObject>();
+        all_object.add(this);
 
+    }
+    // some helper?
     public Coord getRotation(){
         // need type and implementation
         return null;
