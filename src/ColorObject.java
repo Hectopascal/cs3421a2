8808d3cde0968e5f627bdbcdb9637107288c1b2d
Gary Chen <cpldhycxc@gmail.com>
2017-10-19 23:43:33 +1100

    just to make sure i can pull, if error comes up roll back

---
+++ src/ColorObject.java
@@ -0,0 +1,19 @@
+
+
+public class ColorObject {
+    public float x;
+    public float y;
+    public float z;
+    public float o;
+
+    public ColorObject(){
+        this(0.0f,0.0f,0.0f,0.0f);
+    }
+
+    public ColorObject(float x,float y,float z, float o){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.o = o;
+    }
+}
